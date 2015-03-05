14da2519164957d27554749810a1247c81068527
Isaac Getto <isaac.getto@gmail.com>
2015-03-05 18:46:37 -0500

    add draw to views

--- views/base_view.py
+++ views/base_view.py
@@ -3,5 +3,8 @@ class BaseView:
     def __init__(self):
         """ setup the view """
 
+    def draw(self, surface):
+        """ draw the view """
+
     def notify(self, event):
         """ update the view based on the event """
