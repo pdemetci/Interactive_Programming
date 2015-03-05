786063c9775eb08efd2a03fcc294d804955c91b5
Isaac Getto <isaac.getto@gmail.com>
2015-03-05 16:38:55 -0500

    add grid test

--- test/grid_test.py
+++ test/grid_test.py
@@ -2,13 +2,24 @@ import test_helper
 import unittest
 from unittest import TestCase
 
+from grid import Grid
+
 class GridTest(TestCase):
 
+    def testInit(self):
+        g = Grid(4, 5)
+        self.assertEqual(4, g.rows)
+        self.assertEqual(5, g.cols)
+
     def testGet(self):
-        self.fail("not implemented")
+        g = Grid(5, 5)
+        g.set(3, 3, 10)
+        self.assertEqual(10, g.get(3, 3))
 
     def testSet(self):
-        self.fail("not implemented")
+        g = Grid(5, 5)
+        g.set(3, 3, 10)
+        self.assertEqual(10, g.get(3, 3))
 
 if __name__ == "__main__":
     unittest.main()
--- test/test_helper.py
+++ test/test_helper.py
@@ -1,2 +1,9 @@
+import sys
+import os
 import unittest
 from unittest import TestCase
+
+ROOT_DIR = "/home/isaac/Documents/olin/2-semester/softdes/Interactive_Programming"
+
+sys.path.append(ROOT_DIR + "/models")
+sys.path.append(ROOT_DIR + "/controllers")
