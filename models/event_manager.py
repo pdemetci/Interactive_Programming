b47176818b79c40dd8c070c6d802a65375e86630
Isaac Getto <isaac.getto@gmail.com>
2015-03-05 16:57:24 -0500

    implement event manager

--- models/event_manager.py
+++ models/event_manager.py
@@ -2,14 +2,20 @@ class EventManager:
 
     def __init__(self):
         """ create the event manager """
+        self.listeners = []
 
     def registerListener(self, listener):
         """ add a listener to the list of listeners; 
             the listener will be notified when an event occurs """
+        self.listeners.append(listener)
 
     def unregisterListener(self, listener):
         """ remove a listener from the list of listeners;
             the listener will no longer receive notifications """
+        self.listeners.remove(listener)
 
     def post(self, event):
         """ send an event notification to all listeners """
+        for listener in self.listeners:
+            listener.notify(event)
+
