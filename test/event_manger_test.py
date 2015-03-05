d1cbd5dcf32d7045a288c4630f92b32705230c62
Isaac Getto <isaac.getto@gmail.com>
2015-03-04 21:47:44 -0500

    add some empty tests

---
+++ test/event_manger_test.py
@@ -0,0 +1,17 @@
+import test_helper
+import unittest
+from unittest import TestCase
+
+class EventManagerTest(TestCase):
+
+    def testRegisterListener(self):
+        self.fail("not implemented")
+
+    def testUnregisterListern(self):
+        self.fail("not implemented")
+
+    def testPost(self):
+        self.fail("not implemented")
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ test/grid_test.py
@@ -0,0 +1,14 @@
+import test_helper
+import unittest
+from unittest import TestCase
+
+class GridTest(TestCase):
+
+    def testGet(self):
+        self.fail("not implemented")
+
+    def testSet(self):
+        self.fail("not implemented")
+
+if __name__ == "__main__":
+    unittest.main()
