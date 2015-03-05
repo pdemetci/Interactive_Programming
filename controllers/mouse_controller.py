eaf8ba135fa47d162ebe30ed9183e8fda9cbc63b
Isaac Getto <isaac.getto@gmail.com>
2015-03-04 21:50:21 -0500

    create a controller for mouse events

---
+++ controllers/mouse_controller.py
@@ -0,0 +1,9 @@
+import base_controller
+
+class MouseController(BaseController):
+    
+    def __init__(self):
+        """ create the mouse controller """
+
+    def notify(self, event):
+        """ respond to events """
