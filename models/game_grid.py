7bc13c6023906fc3f85a01d76a383ffb2ac16006
Isaac Getto <isaac.getto@gmail.com>
2015-03-04 22:04:19 -0500

    add grids

---
+++ models/game_grid.py
@@ -0,0 +1,10 @@
+import grid
+
+class GameGrid:
+    """ the game grid manages a grid based on events """
+
+    def __init__(self, row, col):
+        """ creates a game grid with given rows and columns """
+
+    def notify(self, event):
+        """ respond to events """
---
+++ models/target_grid.py
@@ -0,0 +1,10 @@
+import grid
+
+class TargetGrid:
+    """ the target grid represents the goal state of the game grid """
+
+    def __init__(self, row, col):
+        """ create the target grid """
+
+    def fill_grid(self, grid):
+        """ fills the grid with the a random target state """
