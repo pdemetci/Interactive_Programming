32e103babe8aa5ca84e0fddf2aacbc5d0d15e412
Isaac Getto <isaac.getto@gmail.com>
2015-03-07 14:19:04 -0500

    setup puzzle game

--- puzzle_game.py
+++ puzzle_game.py
@@ -1,21 +1,48 @@
+import pygame
+
+from event_manager import EventManager
+from game_grid import GameGrid
+from target_grid import TargetGrid
+from game_view import GameView
+from menu_view import MenuView
+from event_manager import EventManager
+from click_event import ClickEvent
+from render_event import RenderEvent
+from get_grid_click_event import GetGridClickEvent
+from draw_game_event import DrawGameEvent
+from grid_click_event import GridClickEvent
+
 class PuzzleGame:
     GAME_WIDTH = 1200
     GAME_HEIGHT = 600
+
+    ROWS = 5
+    COLS = 5
 
     def __init__(self):
         """ create the view and controllers """
         self.done = True
         self.eventManager = EventManager()
-        self.gameGrid = GameGrid(eventManager)
-        self.targetGrid = TargetGrid(eventManager)
-        self.gameView = GameView(eventManager)
+        self.eventManager.registerListener(self)
+
+        self.gameGrid = GameGrid(self.ROWS, self.COLS)
+        self.targetGrid = TargetGrid(self.ROWS, self.COLS)
+
+        self.gameView = GameView()
+        self.menuView = MenuView()
+        self.activeView = None
+        self.surface = None
 
     def start(self):
         """ start the game """
         self.done = False
-        self.surface = pygame.display.set_mode(GAME_WIDTH, GAME_HEIGHT)
-        while not done:
-            main()
+        self.switchView(self.gameView)
+        self.surface = pygame.display.set_mode((self.GAME_WIDTH, self.GAME_HEIGHT))
+        self.eventManager.post(RenderEvent())
+
+        while not self.done:
+            self.main()
+            pygame.time.wait(50)
 
     def stop(self):
         """ stop the game """
@@ -23,15 +50,37 @@ class PuzzleGame:
 
     def main(self):
         """ body of the main loop """
-        for event in pygame.events.get():
-            if event.type == pygame.MOUSEBUTTONDOWN:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.stop()
+                return
+            elif event.type == pygame.MOUSEBUTTONDOWN:
                 x, y = event.pos
-                gameCellGrid = self.gameView.getGameCellGrid(self.surface, self.gameGrid)
-                clickEvent = ClickEvent(x, y, gameCellGrid)
+                clickEvent = ClickEvent(x, y)
+                self.eventManager.post(clickEvent)
 
 
     def notify(self, event):
         """ respond to events """
+        nextEvent = None
+        if type(event) is RenderEvent:
+            surface = self.surface
+            gameGrid = self.gameGrid
+            targetGrid = self.targetGrid
+            nextEvent = DrawGameEvent(surface, gameGrid, targetGrid)
 
-    def switchView(nextView):
+        elif type(event) is GetGridClickEvent:
+            surface = self.surface
+            gameGrid = self.gameGrid
+            nextEvent = GridClickEvent(surface, gameGrid, event.x, event.y)
+
+        if nextEvent:
+            self.eventManager.post(nextEvent)
+            
+
+    def switchView(self, nextView):
         """ change the active view """
+        if self.activeView:
+            self.activeView.removeEventManager()
+        self.activeView = nextView
+        self.activeView.setEventManager(self.eventManager)
