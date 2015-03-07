84062d1633a33ef144624c0d8ea626027de01097
Isaac Getto <isaac.getto@gmail.com>
2015-03-07 14:00:54 -0500

    setup python path

---
+++ setup.py
@@ -0,0 +1,7 @@
+import sys
+
+ROOT_DIR = "/home/isaac/Documents/olin/2-semester/softdes/Interactive_Programming"
+sys.path.append(ROOT_DIR + "/models")
+sys.path.append(ROOT_DIR + "/models/events")
+sys.path.append(ROOT_DIR + "/controllers")
+sys.path.append(ROOT_DIR + "/views")
