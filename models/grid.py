6b63599d42b901721564c0130382590af1b2821a
pdemetci <pinardemetci@gmail.com>
2015-03-07 15:03:42 -0500

    Updating grid model files

--- models/grid.py
+++ models/grid.py
@@ -1,10 +1,27 @@
 class Grid:
+	def __init__(self, rows, cols):
+		""" create a grid with a given number of rowsand columns """
+		self.rows=rows
+		self.cols= cols
+		self.grid = []
+		b=[]
+		for x in range(cols):
+			b.append(0)	
+		for i in range(rows):
+			self.grid.append(b)
 
-    def __init__(self, rows, cols):
-        """ create a grid with a given number of rows and columns """
 
-    def get(self, row, col):
-        """ get the value at a given row and column """
+	def get(self, row, col):
+		""" get the value at a given row and column """
+		return self.grid[row][col]
 
-    def set(self, row, col, val):
-        """ set the cell at the row and column to val """
+	def set(self, row, col, val):
+		""" set the cell at the row and column to val """
+		self.grid[row][col]=val
+
+
+
+
+
+
+    
--- models/target_grid.py
+++ models/target_grid.py
@@ -3,5 +3,17 @@ from grid import Grid
 class TargetGrid(Grid):
     """ the target grid represents the goal state of the game grid """
 
+    def __init__(self, row, col):
+        """ create the target grid """
+    	self.row = row
+    	self.col = col
+    	self.grid= Grid(self.row,self.col)
+    	self.fill_grid()
+
+    def fill_grid(self):
+
     def fill_grid(self, grid):
         """ fills the grid with the a random target state """
+    	for i in range(self.row):
+    		for x in range(self.col):
+    			self.grid.set(i,x,2)
